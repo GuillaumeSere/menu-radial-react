73e895157b141fbddd60e2fcae27f05fc82b99de
Guillaume Sere <guillaumesere@hotmail.fr>
2022-10-04 16:13:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const handleClick = event => {
+    //     // 👇️ toggle isActive state on click
+    event.currentTarget.classList.toggle('active');
+       };
+     
+    return (
+        <div className="App">
+            <ul className="menu" onClick={handleClick}>
+                <div className="toggle" onClick={handleClick}><ion-icon name="add-outline"></ion-icon></div>
+                <li className="li-1" style={{ color: "#ff2972" }}>
+                    <a href="#"><ion-icon name="home-outline"></ion-icon></a>
+                </li>
+                <li className="li-2" style={{ color: "#fee800" }}>
+                    <a href="#"><ion-icon name="person-outline"></ion-icon></a>
+                </li>
+                <li className="li-3" style={{ color: "#04fc43" }}>
+                    <a href="#"><ion-icon name="settings-outline"></ion-icon></a>
+                </li>
+                <li className="li-4" style={{ color: "#fe00f1" }}>
+                    <a href="#"><ion-icon name="mail-outline"></ion-icon></a>
+                </li>
+                <li className="li-5" style={{ color: "#00b0fe" }}>
+                    <a href="#"><ion-icon name="key-outline"></ion-icon></a>
+                </li>
+                <li className="li-6" style={{ color: "#fea600" }}>
+                    <a href="#"><ion-icon name="videocam-outline"></ion-icon></a>
+                </li>
+                <li className="li-7" style={{ color: "#a529ff" }}>
+                    <a href="#"><ion-icon name="game-controller-outline"></ion-icon></a>
+                </li>
+                <li className="li-8" style={{ color: "#01bdab" }}>
+                    <a href="#"><ion-icon name="camera-outline"></ion-icon></a>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
